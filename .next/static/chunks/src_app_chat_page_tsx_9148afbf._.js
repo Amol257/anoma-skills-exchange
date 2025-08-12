424fe2cd97a541259639702ae7f0ee8d72f91ecb
RTm1s <134134802+Amol257@users.noreply.github.com>
2025-08-13 02:27:59 +0530

    Add .next/static/chunks/src_app_chat_page_tsx_9148afbf._.js 1755032279609

---
+++ .next/static/chunks/src_app_chat_page_tsx_9148afbf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_b5dced39._.js",
+  "static/chunks/node_modules_3b8ff446._.js"
+],
+    source: "dynamic"
+});
